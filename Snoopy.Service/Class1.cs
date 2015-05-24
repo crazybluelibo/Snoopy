6a102fe7e83b43ce3980b9dd9f0ebe0e8238cecb
blue <crazy_blue@yeah.net>
2015-05-24 10:47:08 +0800

    first

---
+++ Snoopy.Service/Class1.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snoopy.Service
+{
+    public class Class1
+    {
+    }
+}
