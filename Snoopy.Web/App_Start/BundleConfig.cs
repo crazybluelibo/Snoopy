31651e78f013cdc9b257da2ae88ed8f22ca669a6
blue <crazy_blue@yeah.net>
2015-05-27 20:17:07 +0800

    0

--- Snoopy.Web/App_Start/BundleConfig.cs
+++ Snoopy.Web/App_Start/BundleConfig.cs
@@ -8,24 +8,128 @@ namespace Snoopy.Web
         // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/Scripts/HomeLayout").Include(
+                    "~/Scripts/jquery-1.7.2.min.js",
+                    "~/Scripts/jquery-ui-1.8.21.custom.min.js",
+                    "~/Scripts/jquery.validate.min.js",
+                    "~/Scripts/jquery.unobtrusive-ajax.js",
+                    "~/Scripts/jquery.pjax.js",
+                    "~/Scripts/bootstrap-transition.js",
+                    "~/Scripts/bootstrap-popover.js",
+                    "~/Scripts/bootstrap-alert.js",
+                    "~/Scripts/bootstrap-modal.js",
+                    "~/Scripts/bootstrap-dropdown.js",
+                    "~/Scripts/bootstrap-scrollspy.js",
+                    "~/Scripts/bootstrap-collapse.js",
+                    "~/Scripts/jquery.autocomplete.js",
+                    "~/Scripts/jquery-ui-1.8.11.min.js",
+                    "~/Scripts/jqDnR.js",
+                    "~/Scripts/jqModal.js",
+                    "~/Scripts/jqplot.barRenderer.min.js",
+                    "~/Scripts/jqplot.dateAxisRenderer.min.js",
+                    "~/Scripts/jqplot.categoryAxisRenderer.min.js",
+                    "~/Scripts/jqplot.pointLabels.min.js",
+                    "~/Scripts/jqplot.canvasTextRenderer.min.js",
+                    "~/Scripts/jqplot.canvasAxisTickRenderer.min.js",
+                    "~/Scripts/jqplot.canvasAxisTickRenderer.min.js",
+                    "~/Scripts/jqplot.ohlcRenderer.min.js",
+                    "~/Scripts/jqplot.highlighter.min.js",
+                    "~/Scripts/handlebars-1.0.rc.1.js",
+                    "~/Scripts/jquery.nicescroll.min.js"
+                    ));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Layout").Include(
+                   "~/Scripts/jquery-1.7.2.min.js",
+                   "~/Scripts/jquery-ui-1.8.21.custom.min.js",
+                   "~/Scripts/jquery.validate.min.js",
+                   "~/Scripts/jquery.unobtrusive-ajax.js",
+                   "~/Scripts/jquery.pjax.js",
+                   "~/Scripts/bootstrap-transition.js",
+                   "~/Scripts/bootstrap-alert.js",
+                   "~/Scripts/bootstrap-modal.js",
+                   "~/Scripts/bootstrap-dropdown.js",
+                   "~/Scripts/bootstrap-scrollspy.js",
+                   "~/Scripts/bootstrap-collapse.js",
+                   "~/Scripts/jquery.autocomplete.js",
+                   "~/Scripts/jquery-ui-1.8.11.min.js",
+                   "~/Scripts/jqDnR.js",
+                   "~/Scripts/jqModal.js",
+                   "~/Scripts/handlebars-1.0.rc.1.js",
+                   "~/Scripts/jquery.nicescroll.min.js"
+                   ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+
+            bundles.Add(new ScriptBundle("~/Scripts/PageLayout").Include(
+                      "~/Scripts/jquery-1.7.2.min.js",
+                      "~/Scripts/bootstrap-popover.js",
+                      "~/Scripts/bootstrap-transition.js",
+                      "~/Scripts/bootstrap-alert.js",
+                      "~/Scripts/bootstrap-modal.js",
+                      "~/Scripts/bootstrap-dropdown.js",
+                      "~/Scripts/bootstrap-scrollspy.js",
+                      "~/Scripts/bootstrap-collapse.js",
+                      "~/Scripts/jquery.autocomplete.js",
+                      "~/Scripts/jquery-ui-1.8.21.custom.min.js",
+                      "~/Scripts/jquery.unobtrusive-ajax.js",
+                      "~/Scripts/jquery.pjax.js",
+                      "~/Scripts/jquery-ui-1.8.11.min.js",
+                      "~/Scripts/jqDnR.js",
+                      "~/Scripts/jqModal.js",
+                      "~/Scripts/jquery.jqplot.min.js",
+                      "~/Scripts/jqplot.barRenderer.min.js",
+                      "~/Scripts/jqplot.dateAxisRenderer.min.js",
+                      "~/Scripts/jqplot.categoryAxisRenderer.min.js",
+                      "~/Scripts/jqplot.pointLabels.min.js",
+                      "~/Scripts/jqplot.canvasTextRenderer.min.js",
+                      "~/Scripts/jqplot.canvasAxisTickRenderer.min.js",
+                      "~/Scripts/jqplot.ohlcRenderer.min.js",
+                      "~/Scripts/jqplot.highlighter.min.js",
+                      "~/Scripts/jquery.nicescroll.min.js",
+                      "~/Scripts/handlebars-1.0.rc.1.js"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/Scripts/GoalLayout").Include(
+                     "~/Scripts/jquery-1.7.2.min.js",
+                     "~/Scripts/bootstrap-transition.js",
+                     "~/Scripts/bootstrap-popover.js",
+                     "~/Scripts/bootstrap-alert.js",
+                     "~/Scripts/bootstrap-modal.js",
+                     "~/Scripts/bootstrap-dropdown.js",
+                     "~/Scripts/bootstrap-scrollspy.js",
+                     "~/Scripts/bootstrap-collapse.js",
+                     "~/Scripts/jquery.autocomplete.js",
+                     "~/Scripts/jquery-ui-1.8.21.custom.min.js",
+                     "~/Scripts/jquery.unobtrusive-ajax.js",
+                     "~/Scripts/jquery.pjax.js",
+                     "~/Scripts/jquery-ui-1.8.11.min.js",
+                     "~/Scripts/jqDnR.js",
+                     "~/Scripts/jqModal.js",
+                     "~/Scripts/jquery.jqplot.min.js",
+                     "~/Scripts/jqplot.barRenderer.min.js",
+                    "~/Scripts/jqplot.dateAxisRenderer.min.js",
+                    "~/Scripts/jqplot.categoryAxisRenderer.min.js",
+                    "~/Scripts/jqplot.pointLabels.min.js",
+                    "~/Scripts/jqplot.canvasTextRenderer.min.js",
+                    "~/Scripts/jqplot.canvasAxisTickRenderer.min.js",
+                    "~/Scripts/jqplot.ohlcRenderer.min.js",
+                    "~/Scripts/jqplot.highlighter.min.js",
+                    "~/Scripts/jquery.nicescroll.min.js",
+                    "~/Scripts/handlebars-1.0.rc.1.js"
+                ));
+
+
+            bundles.Add(new StyleBundle("~/Content/CSS").Include(
+                    "~/Content/bootstrap.css",
+                     "~/Content/bootstrap-responsive.css",
+                     "~/Content/HomePage.css",
+                     "~/Content/jquery-ui-1.8.21.custom.css",
+                     "~/Content/jqModal.css",
+                     "~/Content/jquery.jqplot.min.css"
+                     ));
         }
     }
 }
---
+++ Snoopy.Web/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Snoopy.Web.Models
+{
+    public class HomeViewModel
+    {
+
+        //public IEnumerable<NotificationsViewModel> Notification { get; set; }
+        public int? Count { get; set; }
+
+    }
+}
